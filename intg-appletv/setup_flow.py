827c57d35af29ddad1c8d2016c9d82f1067f4b24
Markus Zehnder <business@markuszehnder.ch>
2025-06-05 16:26:18 +0200

    fixup! feat: i18n internationalization with gettext

--- intg-appletv/setup_flow.py
+++ intg-appletv/setup_flow.py
@@ -65,7 +65,7 @@ def setup_data_schema():
         "settings": [
             {
                 "id": "info",
-                "label": _a("Setup proces"),
+                "label": _a("Setup process"),
                 "field": {
                     "label": {
                         "value": _am(
