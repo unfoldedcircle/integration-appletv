badc6cbf67e30311ae03bfe3dbdf02578740da2d
albaintor <118518828+albaintor@users.noreply.github.com>
2025-12-23 11:11:44 -0500

    refactor: Added connection retry when network is not ready (#80)
    - Added connection retry when network is not ready
    - Use old connection configuration for reconnection

--- intg-appletv/tv.py
+++ intg-appletv/tv.py
@@ -68,6 +68,7 @@ BACKOFF_MAX = 30
 BACKOFF_SEC = 2
 ARTWORK_WIDTH = 400
 ARTWORK_HEIGHT = 400
+ERROR_OS_WAIT = 0.5
 
 # pylint: disable=too-many-lines
 
@@ -199,6 +200,7 @@ class AppleTv(interface.AudioListener, interface.DeviceListener):
         self._playback_state = PlaybackState.NORMAL
         self._output_devices_volume: dict[str, float] = {}
         self._volume_level: float = 0.0
+        self._apple_tv_conf: pyatv.interface.BaseConfig | None = None
 
     @property
     def device_config(self) -> AtvDevice:
@@ -456,8 +458,11 @@ class AppleTv(interface.AudioListener, interface.DeviceListener):
 
     async def _connect_once(self) -> None:
         try:
-            if conf := await self._find_atv():
-                await self._connect(conf)
+            # Reuse the latest AppleTV instance (Mac and IP) if defined to avoid a scan
+            if self._apple_tv_conf is None:
+                self._apple_tv_conf = await self._find_atv()
+            if self._apple_tv_conf:
+                await self._connect(self._apple_tv_conf)
         except pyatv.exceptions.AuthenticationError:
             _LOG.warning("[%s] Could not connect: auth error", self.log_id)
             await self.disconnect()
@@ -466,7 +471,22 @@ class AppleTv(interface.AudioListener, interface.DeviceListener):
             pass
         except Exception as err:  # pylint: disable=broad-exception-caught
             _LOG.warning("[%s] Could not connect: %s", self.log_id, err)
-            self._atv = None
+            # OSError(101, 'Network is unreachable') or 10065 for Windows
+            if err.__cause__ and isinstance(err.__cause__, OSError) and err.__cause__.errno in [101, 10065]:
+                _LOG.warning("[%s] Network may not be ready yet %s : retry", self.log_id, err)
+                await asyncio.sleep(ERROR_OS_WAIT)
+                try:
+                    if self._apple_tv_conf is None:
+                        self._apple_tv_conf = await self._find_atv()
+                    if self._apple_tv_conf:
+                        await self._connect(self._apple_tv_conf)
+                except Exception as err2:  # pylint: disable=broad-exception-caught
+                    _LOG.warning("[%s] Could not connect: %s", self.log_id, err2)
+                    self._atv = None
+            else:
+                # Reset AppleTV configuration in case this is the wrong conf (changed Mac or IP)
+                self._apple_tv_conf = None
+                self._atv = None
 
     async def _connect(self, conf: pyatv.interface.BaseConfig) -> None:
         # We try to connect with all the protocols.
