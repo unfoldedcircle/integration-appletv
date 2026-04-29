86803e46161b30d600bb0f21d746476339cdd1e5
albaintor <118518828+albaintor@users.noreply.github.com>
2026-04-29 11:53:09 +0200

    Linting

--- intg-appletv/media_player.py
+++ intg-appletv/media_player.py
@@ -13,7 +13,6 @@ from typing import Any
 import tv
 from config import AppleTVEntity, AtvDevice
 from const import filter_attributes
-from hid.consumer_control_code import ConsumerControlCode
 from ucapi import MediaPlayer, StatusCodes, media_player
 from ucapi.media_player import (
     Attributes,
@@ -24,6 +23,7 @@ from ucapi.media_player import (
 )
 
 from hid import UsagePage
+from hid.consumer_control_code import ConsumerControlCode
 
 _LOG = logging.getLogger(__name__)
 # Experimental features, don't seem to work / supported (yet) with ATV4
